3f5ab3e2adf04fc61099fe14bd337b0b529dd0a9
Ian Huntington <ihuntington85@gmail.com>
2013-08-09 14:08:31 +0100

    Website updates

--- www/js/ui.js
+++ www/js/ui.js
@@ -1,5 +1,112 @@
 var barbican = window.barbican || {};
 
+/**
+ * clockWatcher - clock timing script
+ */
+
+(function(document) {
+    
+    var $target = $('time'),
+        $timeTarget = $('#test'),
+        $tarAttr = $('time').attr('datetime'),
+        time,
+        lastTimeChange,
+        pollingChanged;
+
+    window.clockWatcher = window.clockWatcher || {};
+    var clockWatcher = window.clockWatcher;
+
+    window.requestAnimFrame = (function() {
+        return  window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+        
+                function( callback ) {
+                    window.setTimeout(callback, 1000 / 60);
+                };
+    })();
+
+    clockWatcher.log = function() {
+        if(clockWatcher.debug) {
+            console.log.apply(console, arguments);
+        }
+    };
+
+    clockWatcher.init = function(format, debug) {
+
+        clockWatcher.log("ClockWatcher running");
+
+        // cache
+        clockWatcher.format = format;
+        clockWatcher.lastPoll = new Date().getTime();
+        clockWatcher.debug = !!debug;
+
+        $.ajax({
+            url: "/time",
+            success: function(data) {
+                clockWatcher.processResponse(data);
+                clockWatcher.delay = (60 - parseInt(data.split('|')[0].split(':')[2], 10)) * 1000;
+                clockWatcher.draw();
+            }
+        });
+    };
+
+    clockWatcher.draw = function() {
+
+        var currentTime = new Date().getTime();
+
+        if((currentTime - clockWatcher.lastPoll) > clockWatcher.delay) {
+            clockWatcher.log("ajax", currentTime);
+            clockWatcher.ajaxRequest();
+            clockWatcher.lastPoll = currentTime;
+            clockWatcher.delay = 60000;
+        }
+
+        requestAnimFrame(clockWatcher.draw);
+    };
+
+    clockWatcher.ajaxRequest = function() {
+        $.ajax({
+            url: "/time",
+            success: clockWatcher.processResponse
+        });
+    };
+
+    clockWatcher.processResponse = function(data) {
+        clockWatcher.log(data);
+        var items = data.split('|');
+        
+        clockWatcher.displayTime(items[0]);
+        clockWatcher.playAudio(items[1]);
+    };
+
+    clockWatcher.displayTime = function(timeString) {
+        var time;
+        switch(clockWatcher.format) {
+        case "HH":
+            time = timeString.split(":")[0];
+            break;
+        case "mm":
+            time = timeString.split(":")[1];
+            break;
+        default:
+            time = timeString.split(":")[0]+":"+timeString.split(":")[1];
+        }
+        $target.html(time);
+    };
+
+    clockWatcher.playAudio = function(audio) {
+        var player = $('#audioPlayer'),
+            playerSource = $('#audioPlayer source');
+
+        playerSource[0].src = audio;
+        player[0].load();
+    };
+
+})(document);
+
 barbican.ui = function () {
 
 	var ctas = $('.cta_collection a');
@@ -24,6 +131,8 @@ barbican.ui = function () {
 
 	ctas.on('click', onClickHandler);
 	close.on('click', onClickHandler);
+
+	console.log(clockWatcher.init());
 };
 
 barbican.sound = function () {
